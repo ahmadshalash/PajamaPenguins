5a6e91ec4f9ca1a562c6f71d7d8c1f3e1c5edca1
Skye <gskye@aol.com>
2015-02-06 13:13:08 -0500

    Created iceberg and penguin art

--- PajamaPenguins_Shared/PPGameScene.m
+++ PajamaPenguins_Shared/PPGameScene.m
@@ -10,11 +10,13 @@
 #import "SSKGraphicsUtils.h"
 
 typedef enum {
+    playerLayer,
     menuLayer,
 }Layers;
 
 @interface PPGameScene()
-@property (nonatomic) SKNode *menuNode;
+@property (nonatomic) SKNode *playerLayerNode;
+@property (nonatomic) SKNode *menuLayerNode;
 @end
 
 @implementation PPGameScene
@@ -29,53 +31,80 @@ typedef enum {
 
 - (void)createScene {
     self.backgroundColor = SKColorWithRGB(6, 220, 220);
-    [self createMenu];
+
+    [self createBackgroundLayer];
+    [self createPlayerLayer];
+    [self createMenuLayer];
 }
 
-- (void)createMenu {
-    self.menuNode = [SKNode new];
-    [self.menuNode setZPosition:menuLayer];
-    [self.menuNode setName:@"menu"];
-    [self addChild:self.menuNode];
+- (void)createMenuLayer {
+    self.menuLayerNode = [SKNode node];
+    [self.menuLayerNode setZPosition:menuLayer];
+    [self.menuLayerNode setName:@"menu"];
+    [self addChild:self.menuLayerNode];
     
-    SKNode *startButtonNode = [SKNode new];
+    SKNode *startButtonNode = [SKNode node];
     [startButtonNode setName:@"startButtonNode"];
-    [self.menuNode addChild:startButtonNode];
+    [self.menuLayerNode addChild:startButtonNode];
     
     SSKButton *startButton = [[SSKButton alloc] initWithIdleTexture:[sTextures objectAtIndex:15] selectedTexture:[sTextures objectAtIndex:16]];
     [startButton setTouchUpInsideTarget:self selector:@selector(startButtonTouchUpInside)];
     [startButton setName:@"startButton"];
     [startButton setPosition:CGPointMake(self.size.width/2, self.size.height/4)];
-    [startButton setXScale:8];
-    [startButton setYScale:4];
+    [startButton setXScale:10];
+    [startButton setYScale:6];
     [startButtonNode addChild:startButton];
-//    
-//    SKLabelNode *startButtonLabel = [self createNewLabelWithText:@"Dive !"];
-//    [startButtonLabel setPosition:startButton.position];
-//    [startButtonNode addChild:startButtonLabel];
+    
+    SKLabelNode *startButtonLabel = [self createNewLabelWithText:@"Dive !"];
+    [startButtonLabel setPosition:startButton.position];
+    [startButtonNode addChild:startButtonLabel];
+}
+
+- (void)createBackgroundLayer {
+}
+
+- (void)createPlayerLayer {
+    self.playerLayerNode = [SKNode node];
+    [self.playerLayerNode setZPosition:playerLayer];
+    [self.playerLayerNode setName:@"playerLayer"];
+    [self addChild:self.playerLayerNode];
+    
+    SKSpriteNode *platform = [SKSpriteNode spriteNodeWithTexture:[sTextures objectAtIndex:1]];
+    [platform setScale:10];
+    [platform setPosition:CGPointMake(self.size.width/2, self.size.height/2)];
+    [platform setName:@"platform"];
+    [self.playerLayerNode addChild:platform];
+    
+    SKSpriteNode *player = [SKSpriteNode spriteNodeWithTexture:[sTextures objectAtIndex:0]];
+    [player setScale:5];
+    [player setPosition:CGPointMake(platform.position.x, platform.position.y + player.size.height)];
+    [player setName:@"player"];
+    [player setZPosition:playerLayer];
+    [self.playerLayerNode addChild:player];
+    
 }
 
 #pragma mark - Button Methods
 - (void)startButtonTouchUpInside {
-//    SKNode *menu = [self childNodeWithName:@"menu"];
-//    SKNode *buttonNode = [menu childNodeWithName:@"startButtonNode"];
-//    SKNode *button = [buttonNode childNodeWithName:@"startButton"];
-//    
-//    if (!menu.hasActions) {
-//        [button setUserInteractionEnabled:NO];
-//        [menu runAction:[SKAction fadeOutWithDuration:.5] completion:^{
-//            [menu removeFromParent];
-//        }];
-//    }
+    SKNode *menu = [self childNodeWithName:@"menu"];
+    SKNode *buttonNode = [menu childNodeWithName:@"startButtonNode"];
+    SKNode *button = [buttonNode childNodeWithName:@"startButton"];
+    
+    if (!menu.hasActions) {
+        [button setUserInteractionEnabled:NO];
+        [menu runAction:[SKAction fadeOutWithDuration:.5] completion:^{
+            [menu removeFromParent];
+        }];
+    }
 }
 
 #pragma mark - Convenience
 - (SKLabelNode *)createNewLabelWithText:(NSString*)text {
-    SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"ChalkDuster"];
+    SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Verdana"];
     [label setHorizontalAlignmentMode:SKLabelHorizontalAlignmentModeCenter];
     [label setVerticalAlignmentMode:SKLabelVerticalAlignmentModeCenter];
     [label setText:text];
-    [label setFontSize:25];
+    [label setFontSize:35];
     return label;
 }
 
